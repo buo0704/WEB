a9287c5d96f82e21d856e595bb9532d36db222c6
이이섭 <aa990724@naver.com>
2025-09-15 11:03:23 +0900

    자바스크립트를 사용하여 반지름이 10인 원을 둘레 구하기

---
+++ JavaScript/연산/variable_use.js
@@ -0,0 +1,12 @@
+// 반지름 10인 원의 둘레 구하기
+
+// 변수를 선언 및 초기화
+// let radius = 10;
+// let pi = 3.14159265;
+
+// let를 const(상수)로 변경
+const radius = 10;
+const pi = 3.14159265
+
+// 출력
+alert(2 * radius * pi);
